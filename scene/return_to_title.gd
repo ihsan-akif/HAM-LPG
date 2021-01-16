1ed3488021e63a153acab5bc4dc478ca6af8d266
unknown <ihsan.akif@gmail.com>
2021-01-16 21:54:45 +0800

    Learning Section added

--- scene/return_to_title.gd
+++ scene/return_to_title.gd
@@ -2,3 +2,18 @@ extends Control
 
 func _on_Button_pressed() -> void:
 	get_tree().change_scene("res://main_menu/MainMenu.tscn")
+
+
+func _on_TextureButton_pressed() -> void:
+	get_tree().change_scene("res://main_menu/MainMenu.tscn")
+
+
+func _on_PlayButton_pressed():
+	if $Panel/VideoPlayer.paused:
+		$Panel/VideoPlayer.paused = false
+	else:
+		$Panel/VideoPlayer.play()
+
+
+func _on_PauseButton_pressed() -> void:
+	$Panel/VideoPlayer.paused = true
