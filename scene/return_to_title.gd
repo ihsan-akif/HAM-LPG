481f6a95061a6b25a136c37ed1ba5275668da23d
unknown <ihsan.akif@gmail.com>
2021-01-22 23:43:35 +0800

    Update UI

--- scene/return_to_title.gd
+++ scene/return_to_title.gd
@@ -17,3 +17,15 @@ func _on_PlayButton_pressed():
 
 func _on_PauseButton_pressed() -> void:
 	$Panel/VideoPlayer.paused = true
+
+
+func _on_NextPageButton_pressed() -> void:
+	get_tree().change_scene("res://learning_section/LearningSection2.tscn")
+
+
+func _on_PreviousPageButton_pressed() -> void:
+	get_tree().change_scene("res://learning_section/LearningSection.tscn")
+
+
+func _on_TextureButton6_pressed() -> void:
+	get_tree().change_scene("res://trivia_question/TriviaQuestion2.tscn")
