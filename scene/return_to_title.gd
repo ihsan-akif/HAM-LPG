97e72dab2d6adbbd19c0cd150c359b18598c7346
unknown <ihsan.akif@gmail.com>
2021-03-23 13:03:43 +0800

    LS Update

--- scene/return_to_title.gd
+++ scene/return_to_title.gd
@@ -20,12 +20,23 @@ func _on_PauseButton_pressed() -> void:
 
 
 func _on_NextPageButton_pressed() -> void:
-	get_tree().change_scene("res://learning_section/LearningSection2.tscn")
+	get_tree().change_scene("res://learning_section/LearningSectionPage2.tscn")
 
 
 func _on_PreviousPageButton_pressed() -> void:
-	get_tree().change_scene("res://learning_section/LearningSection.tscn")
+	get_tree().change_scene("res://learning_section/LearningSectionPage1.tscn")
 
+func _on_Q2NextBtn_pressed() -> void:
+	get_tree().change_scene("res://trivia_question/TriviaQuestion3.tscn")
 
-func _on_TextureButton6_pressed() -> void:
+func _on_Q1NextBtn_pressed() -> void:
 	get_tree().change_scene("res://trivia_question/TriviaQuestion2.tscn")
+
+func _on_Q3NextBtn_pressed() -> void:
+	get_tree().change_scene("res://trivia_question/TriviaQuestion4.tscn")
+
+func _on_Q4NextBtn_pressed() -> void:
+	get_tree().change_scene("res://trivia_question/TriviaQuestion5.tscn")
+
+func _on_Q5NextBtn_pressed() -> void:
+	get_tree().change_scene("res://trivia_question/Congrats.tscn")
