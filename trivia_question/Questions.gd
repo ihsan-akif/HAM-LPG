c2fee4ea756310809cdde64b97b57ef9117ea55c
ihsan-akif <ihsan.akif@gmail.com>
2021-04-07 20:42:50 +0800

    Question Update

--- trivia_question/Questions.gd
+++ trivia_question/Questions.gd
@@ -6,18 +6,11 @@ var score : int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#trivia_question()
-	#print(score)
 	pass
 
 
 func _process(delta):
 	pass
-
-
-# Gameplay
-func trivia_question() -> void:
-	print(score)
 
 
 func _on_AnswerA1Btn_pressed():
@@ -48,3 +41,117 @@ func _on_AnswerD1Btn_pressed():
 	get_node("Question1").hide()
 	get_node("Question2").show()
 	
+
+
+func _on_AnswerA2Btn_pressed():
+	get_node("Question2/AnswerA2Btn").disabled = true
+	highScore -= 5
+	print(highScore)
+
+
+func _on_AnswerB2Btn_pressed():
+	get_node("Question2/AnswerA2Btn").disabled = true
+	get_node("Question2/AnswerB2Btn").disabled = true
+	get_node("Question2/AnswerC2Btn").disabled = true
+	get_node("Question2/AnswerD2Btn").disabled = true
+	print(highScore)
+	yield(get_tree().create_timer(2.0), "timeout")
+	get_node("Question2").hide()
+	get_node("Question3").show()
+
+func _on_AnswerC2Btn_pressed():
+	get_node("Question2/AnswerC2Btn").disabled = true
+	highScore -= 5
+	print(highScore)
+
+
+func _on_AnswerD2Btn_pressed():
+	get_node("Question2/AnswerD2Btn").disabled = true
+	highScore -= 5
+	print(highScore)
+
+
+func _on_AnswerA3Btn_pressed():
+	get_node("Question3/AnswerA3Btn").disabled = true
+	highScore -= 5
+	print(highScore)
+
+
+func _on_AnswerB3Btn_pressed():
+	get_node("Question3/AnswerB3Btn").disabled = true
+	highScore -= 5
+	print(highScore)
+
+
+func _on_AnswerC3Btn_pressed():
+	get_node("Question3/AnswerA3Btn").disabled = true
+	get_node("Question3/AnswerB3Btn").disabled = true
+	get_node("Question3/AnswerC3Btn").disabled = true
+	get_node("Question3/AnswerD3Btn").disabled = true
+	print(highScore)
+	yield(get_tree().create_timer(2.0), "timeout")
+	get_node("Question3").hide()
+	get_node("Question4").show()
+
+
+func _on_AnswerD3Btn_pressed():
+	get_node("Question3/AnswerD3Btn").disabled = true
+	highScore -= 5
+	print(highScore)
+
+
+func _on_AnswerA4Btn_pressed():
+	get_node("Question4/AnswerA4Btn").disabled = true
+	highScore -= 5
+	print(highScore)
+
+
+func _on_AnswerB4Btn_pressed():
+	get_node("Question4/AnswerB4Btn").disabled = true
+	highScore -= 5
+	print(highScore)
+
+
+func _on_AnswerC4Btn_pressed():
+	get_node("Question4/AnswerC4Btn").disabled = true
+	highScore -= 5
+	print(highScore)
+
+
+func _on_AnswerD4Btn_pressed():
+	get_node("Question4/AnswerA4Btn").disabled = true
+	get_node("Question4/AnswerB4Btn").disabled = true
+	get_node("Question4/AnswerC4Btn").disabled = true
+	get_node("Question4/AnswerD4Btn").disabled = true
+	print(highScore)
+	yield(get_tree().create_timer(2.0), "timeout")
+	get_node("Question4").hide()
+	get_node("Question5").show()
+
+
+func _on_AnswerA5Btn_pressed():
+	get_node("Question5/AnswerA5Btn").disabled = true
+	get_node("Question5/AnswerB5Btn").disabled = true
+	get_node("Question5/AnswerC5Btn").disabled = true
+	get_node("Question5/AnswerD5Btn").disabled = true
+	print(highScore)
+	yield(get_tree().create_timer(2.0), "timeout")
+	get_node("Question5").hide()
+	# Next question show
+
+
+func _on_AnswerB5Btn_pressed():
+	get_node("Question5/AnswerB5Btn").disabled = true
+	highScore -= 5
+	print(highScore)
+
+func _on_AnswerC5Btn_pressed():
+	get_node("Question5/AnswerC5Btn").disabled = true
+	highScore -= 5
+	print(highScore)
+
+
+func _on_AnswerD5Btn_pressed():
+	get_node("Question5/AnswerD5Btn").disabled = true
+	highScore -= 5
+	print(highScore)
