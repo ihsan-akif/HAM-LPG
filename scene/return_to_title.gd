b78cd8f9be344b901ad8a4999b07dec8f302f855
ihsan-akif <ihsan.akif@gmail.com>
2021-06-11 22:36:54 +0800

    Learning section update

--- scene/return_to_title.gd
+++ scene/return_to_title.gd
@@ -23,21 +23,65 @@ func _on_NextPage1Button_pressed():
 	get_tree().change_scene("res://learning_section/LearningSectionPage2.tscn")
 
 
-func _on_NextPage2Button_pressed():
-	get_tree().change_scene("res://learning_section/LearningSectionPage3.tscn")
-
-
 func _on_PreviousPage2Button_pressed():
 	get_tree().change_scene("res://learning_section/LearningSectionPage1.tscn")
 
 
-func _on_NextPage3Button_pressed():
-	get_tree().change_scene("res://learning_section/LearningSectionPage4.tscn")
+func _on_NextPage2Button_pressed():
+	get_tree().change_scene("res://learning_section/LearningSectionPage3.tscn")
 
 
 func _on_PreviousPage3Button_pressed():
 	get_tree().change_scene("res://learning_section/LearningSectionPage2.tscn")
 
 
+func _on_NextPage3Button_pressed():
+	get_tree().change_scene("res://learning_section/LearningSectionPage4.tscn")
+
+
 func _on_PreviousPage4Button_pressed():
 	get_tree().change_scene("res://learning_section/LearningSectionPage3.tscn")
+
+
+func _on_NextPage4Button_pressed():
+	get_tree().change_scene("res://learning_section/LearningSectionPage5.tscn")
+
+
+func _on_PreviousPage5Button_pressed():
+	get_tree().change_scene("res://learning_section/LearningSectionPage4.tscn")
+
+
+func _on_NextPage5Button_pressed():
+	get_tree().change_scene("res://learning_section/LearningSectionPage6.tscn")
+
+
+func _on_PreviousPage6Button_pressed():
+	get_tree().change_scene("res://learning_section/LearningSectionPage5.tscn")
+
+
+func _on_NextPage6Button_pressed():
+	get_tree().change_scene("res://learning_section/LearningSectionPage7.tscn")
+
+
+func _on_PreviousPage7Button_pressed():
+	get_tree().change_scene("res://learning_section/LearningSectionPage6.tscn")
+
+
+func _on_NextPage7Button_pressed():
+	get_tree().change_scene("res://learning_section/LearningSectionPage8.tscn")
+
+
+func _on_PreviousPage8Button_pressed():
+	get_tree().change_scene("res://learning_section/LearningSectionPage7.tscn")
+
+
+func _on_NextPage8Button_pressed():
+	get_tree().change_scene("res://learning_section/LearningSectionPage9.tscn")
+
+
+func _on_PreviousPage9Button_pressed():
+	get_tree().change_scene("res://learning_section/LearningSectionPage8.tscn")
+
+
+func _on_NextPage9Button_pressed():
+	pass # Replace with function body.
